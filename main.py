72b221d8b928d74522a291919ab2fa566611ea48
Sina Ghandi <ghandisina@gmail.com>
2023-07-28 19:10:22 -0500

    added a !count command, and the bot automatically, correctly checks every message for a wonky sokka haiku (5-7-6). Sometimes inaccurate due to limitations of syllapy, for example, worse ending with double 'e' (puree) will be counted as one syllable too few.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
 import discord
 import syllapy
 from discord.ext import commands
@@ -13,16 +12,58 @@ intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 
+def count_sentence(sentence):
+    syllables = 0
+    words = sentence.split()
+    for word in words:
+        syllables += syllapy.count(word)
+    return syllables
+
+
 def is_haiku(text):
-    lines = text.splitlines()
-    if len(lines) != 3:
+    words = text.split()
+    syllables_list = []
+    for word in words:
+        syllables = syllapy.count(word)
+        syllables_list.append((word, syllables))
+    if sum(word[1] for word in syllables_list) != 18:
         return False
-    return syllapy.count(lines[0]) == 5 and syllapy.count(lines[1]) == 7 and syllapy.count(lines[2]) == 6
+
+    syllables_current = 0
+    line1 = []
+    line2 = []
+    line3 = []
+
+    for syllables in syllables_list:
+        line1.append(syllables[0])
+        syllables_current += syllables[1]
+        if syllables_current > 5:
+            return False
+        if syllables_current == 5:
+            syllables_list = syllables_list[syllables_list.index(syllables) + 1:]
+            break
+
+    syllables_current = 0
+    for syllables in syllables_list:
+        line2.append(syllables[0])
+        syllables_current += syllables[1]
+        if syllables_current > 7:
+            return False
+        if syllables_current == 7:
+            syllables_list = syllables_list[syllables_list.index(syllables) + 1:]
+            break
+
+    for syllables in syllables_list:
+        line3.append(syllables[0])
+        syllables_current += syllables[1]
+
+    return [True, [line1, line2, line3]]
 
 
 @bot.command()
 async def frenchfry(context):
     await context.reply("I love French Fry!")
+
 
 @bot.command()
 async def hello(context):
@@ -32,7 +73,7 @@ async def hello(context):
 @bot.command()
 async def count(context, *args):
     arguments = " ".join(args)
-    await context.reply(f'{arguments} has {syllapy.count(arguments)} syllables.')
+    await context.reply(f'{arguments} has {count_sentence(arguments)} syllables.')
 
 
 @bot.event
@@ -42,8 +83,12 @@ async def on_message(message):
         return
 
     content = message.content
-    if is_haiku(content):
-        await message.channel.send(f"Detected a Haiku from {message.author.mention}:\n{content}")
+    haiku = is_haiku(content)
+    if haiku:
+        line1 = " ".join(haiku[1][0])
+        line2 = " ".join(haiku[1][1])
+        line3 = " ".join(haiku[1][2])
+        await message.channel.send(f"\n{line1}\n{line2}\n{line3}")
 
     await bot.process_commands(message)  # to allow processing commands in addition to the on_message event
 
