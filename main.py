08c4803d24730291100e659fc7f40435e02f65ed
Sina Ghandi <ghandisina@gmail.com>
2023-08-03 14:24:41 -0500

    removes count_sentences fxn (DRY), added comments

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import discord
 import syllapy
 from discord.ext import commands
@@ -9,20 +5,22 @@ from config import BOT_TOKEN
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix="!", intents=intents)
-
-
-def count_sentence(sentence):
-    syllables = 0
-    words = sentence.split()
-    for word in words:
-        syllables += syllapy.count(word)
-    return syllables
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 
 def is_haiku(text):
+    """Determines if the provided text forms a 5-7-6 haiku.
+
+    Args:
+        text (str): The text to be checked.
+
+    Returns:
+        Tuple[bool, List[List[str]]]: A tuple containing a boolean indicating if the text is a haiku,
+        and a list of the three lines if it is.
+    """
     words = text.split()
     syllables_list = []
+    # Checks if total syllables in text equals 18
     for word in words:
         syllables = syllapy.count(word)
         syllables_list.append((word, syllables))
@@ -30,10 +28,12 @@ def is_haiku(text):
         return False
 
     syllables_current = 0
+    # will be filled with respective lines of the haiku
     line1 = []
     line2 = []
     line3 = []
 
+    # Checks if there is a break at 5 total syllables to form first line
     for syllables in syllables_list:
         line1.append(syllables[0])
         syllables_current += syllables[1]
@@ -43,6 +43,7 @@ def is_haiku(text):
             syllables_list = syllables_list[syllables_list.index(syllables) + 1:]
             break
 
+    # Checks if there is a break at 12 total syllables to split second and third lines
     syllables_current = 0
     for syllables in syllables_list:
         line2.append(syllables[0])
@@ -72,18 +73,32 @@ async def hello(context):
 
 @bot.command()
 async def count(context, *args):
-    arguments = " ".join(args)
-    await context.reply(f'{arguments} has {count_sentence(arguments)} syllables.')
+    """Command that counts and replies with the number of syllables in the given text.
+
+    Args:
+        context: The discord.py context of the command.
+        *args: The text to be counted.
+    """
+    sentence = " ".join(args)
+    syllables = 0
+    words = sentence.split()
+    for word in words:
+        syllables += syllapy.count(word)
+    await context.reply(f'{arguments} has {syllables} syllables.')
 
 
 @bot.event
 async def on_message(message):
+    """Event handler for new messages. Checks if the message is a haiku and sends a formatted version if it is.
+
+    Args:
+        message: The incoming message.
+    """
     # Ignore messages from the bot
     if message.author == bot.user:
         return
 
-    content = message.content
-    haiku = is_haiku(content)
+    haiku = is_haiku(message.content)
     if haiku:
         line1 = " ".join(haiku[1][0])
         line2 = " ".join(haiku[1][1])
